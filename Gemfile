8185ff9d1bf7c95dcc8b3af70e7b0e3c00aefcf6
MStines007 <mstines007@gmail.com>
2015-06-13 19:41:47 -0400

    Initial commit. App functions properly.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,7 @@ source "https://rubygems.org"
 
 gem 'pry'
 gem 'sinatra'
+gem 'giphy', '~> 1.2.0'
+gem 'twilio-ruby'
+gem 'dotenv'
 
--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,38 @@
 require 'bundler'
 Bundler.require
+require 'twilio-ruby' 
+require 'giphy'
+require 'dotenv'
 
 class ApplicationController < Sinatra::Base
+
+    account_sid ||= ENV["account_sid"]
+    auth_token ||= ENV["auth_token"]
+    twilio_number ||= ENV["twilio_number"]
 
   get '/' do
     erb :index
   end
+
+  post '/' do
+    @recipient = params[:recipient]
+    @keyword = params[:keyword]
+    @number = params[:number]
+    @message = params[:message]
+    @sender = params[:sender]
+    @greeting = "Hey #{@recipient}!, #{@message} - Sent by #{@sender}"
+    @gif = Giphy.random(tag=@keyword).image_original_url.to_s
+    binding.pry
+
+    @client = Twilio::REST::Client.new account_sid, auth_token 
+ 
+    @client.account.messages.create({
+    :from => twilio_number, 
+    :to => @number,
+    :body => @greeting, 
+    :media_url => @gif })
+    
+    erb :result
+  end 
 
 end
